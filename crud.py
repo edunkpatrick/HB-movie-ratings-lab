e7f42bef1480612289908c849d18cfeab5d6be44
Erin Dunkley <eskilpatrick@gmail.com>
2023-01-19 16:56:56 -0800

    Part 3 Complete

--- crud.py
+++ crud.py
@@ -11,12 +11,28 @@ def create_user(email, password):
     user = User(email=email, password=password)
 
     return user
+
+def create_user_list():
+    # returns a list of users
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    # returns a user
+    return User.query.get(user_id) 
     
 def create_movie(title, overview, release_date, poster_path):
 
     movie = Movie(title=title, overview=overview, release_date=release_date, poster_path=poster_path)
 
     return movie
+
+def get_all_movies():
+    # returns all movies
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    # returns a movie by id
+    return Movie.query.get(movie_id)
 
 def create_rating(user, movie, score):
 
--- server.py
+++ server.py
@@ -1,13 +1,56 @@
 """Server for movie ratings app."""
 
-from flask import Flask
+from flask import (Flask, render_template, request, flash, session, redirect)
+
+from model import connect_to_db, db
+
+import crud
+
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = 'dev'
+app.jinja_env.undefined = StrictUndefined
 
 app = Flask(__name__)
 
 
 # Replace this with routes and view functions!
+# create homepage
+@app.route('/')
+def homepage():
+    """Homepage"""
 
+    return render_template('homepage.html', )
+
+@app.route('/movies')
+def view_movies():
+    """List of movies"""
+    movies = crud.get_all_movies()
+
+    return render_template('view_movies.html', movies=movies)
+
+@app.route("/movies/<movie_id>")
+def show_movie(movie_id):
+    """Show details of a movie"""
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template('movie_details.html', movie=movie)
+
+@app.route("/users")
+def view_users():
+
+    users = crud.create_user_list()
+
+    return render_template('view_users.html', users=users)
+
+@app.route("/user/<user_id>")
+def show_user(user_id):
+
+    user = crud.get_user_by_id(user_id)
+
+    return render_template('user_details.html', user=user)
 
 if __name__ == "__main__":
-
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)

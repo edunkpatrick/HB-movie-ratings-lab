94e37a453c8dd806929472c50391b4dac5eb3e55
Erin Dunkley <eskilpatrick@gmail.com>
2023-01-20 17:01:24 -0800

    issues with secret key

--- server.py
+++ server.py
@@ -21,7 +21,56 @@ app = Flask(__name__)
 def homepage():
     """Homepage"""
 
-    return render_template('homepage.html', )
+    return render_template('homepage.html')
+
+@app.route('/login', methods=['POST'])
+def login_authentication():
+    """Verifies user log in"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # check database for entered email
+    # verify password stored with that email matches
+
+    # users can have same password, search by email
+    # authentication = crud.get_user_by_password(email)
+
+    user = crud.get_user_by_email(email)
+
+    if user:
+        if user.password == password:
+            session["email"] = email
+        else:
+            flash("Incorrect password")
+
+    # if authentication:
+    #     flash("Incorrect password")
+    # else:
+    #     session["email"] = email
+    # flash("Logged in!")
+
+    return redirect('/')
+
+@app.route('/users', methods = ['POST'])
+def create_user():
+    """Create a user"""
+    
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+
+    # check if a user with the email from request.form already exists
+    if user:
+        flash("Account with that email exists, please try again.")
+    # if user does not exist with that email, commit user to database
+    else:
+        user=crud.create_user(email, password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created successfully, please log in.")
+
+    return redirect('/')
 
 @app.route('/movies')
 def view_movies():
